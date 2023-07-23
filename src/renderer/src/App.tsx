95d33964ee7c897e46c5bffaad6d027ac7e5ff93
lxfriday <liu3248184446@outlook.com>
2023-07-23 12:33:16 +0800

    chore: format

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,10 @@
-import Navbar from '@renderer/components/common/Navbar';
+import Navbar from '@renderer/components/common/Navbar'
 
 function App(): JSX.Element {
   return (
-    <div className="border bg-gray-100 h-full w-full flex">
+    <div className="flex h-full w-full border bg-gray-100">
       <Navbar />
-      <div className='flex-1'></div>
+      <div className="flex-1"></div>
     </div>
   )
 }
